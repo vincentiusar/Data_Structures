f701c3e727e3e71963f5cda799c3c224349c28f3
Vincentius Ar <58662886+vincentiusar@users.noreply.github.com>
2020-02-28 14:28:53 +0700

    Update list.h

--- DLL/list.h
+++ DLL/list.h
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+typedef int infotype;
 typedef struct elmlist *address;
 struct elmlist {
     int info;
@@ -14,9 +15,17 @@ struct List {
     address last;
 };
 
-void insert_first(List &l, address p);
-void insert_after(List &l, address p);
-void delete_first(List &l, address p);
-void delete_after(List &l, address p, address prec);
-void insert_last(List &l, address p);
-void delete_last(List &l, address p);
+bool isEmpty(List L);
+void createList(List &L);
+void createNewElmt(infotype X, address &P);
+void insertFirst(List &L, address P);
+void insertAfter(List &L, address Prec, address P);
+void insertLast(List &L, address P);
+void deleteFirst(List &L, address P);
+void deleteAfter(List &L, address Prec, address P);
+void deleteLast(List &L, address P);
+void concat(List L1, List L2, List &L3);
+double median(List L);
+void PrintInfo(List L);
+address findElm(List L, int x);
+void reversePrint(List L);
