53a30cc5a74028754671f3104d3045cc4d6a80e0
Vincentius Ar <58662886+vincentiusar@users.noreply.github.com>
2020-02-28 14:25:39 +0700

    update main.cpp

--- DLL/main.cpp
+++ DLL/main.cpp
@@ -1,5 +1,39 @@
 #include "list.h"
 
 int main() {
-    //belum dibuat.. mohon maaf.. :'(
+    List L;
+    address P, Pointer;
+    createList(L);
+    createNewElmt('a', P);
+    insertFirst(L, P);
+    printInfo(L);
+    createNewElmt('t', P);
+    insertFirst(L, P);
+    printInfo(L);
+    createNewElmt('t', P);
+    insertAfter(findElm(L, 'a'), P);
+    printInfo(L);
+    createNewElmt('c', P);
+    insertAfter(findElm(L, 't'), P);
+    printInfo(L);
+    createNewElmt('a', P);
+    insertAfter(L.first -> prev, P);
+    printInfo(L);
+    createNewElmt('s', P);
+    insertAfter(L.first -> prev, P);
+    printInfo(L);
+    createNewElmt('c', P);
+    insertAfter(L.first -> prev, P);
+    printInfo(L);
+    createNewElmt('a', P);
+    insertAfter(L.first -> prev, P);
+    printInfo(L);
+    cout << countWord("cat", L) << endl;
+    deleteFirst(L, P);
+    cout << "Delete first elm : ";
+    printInfo(L);
+    deleteAfter(findElm(L, 'a'), P);
+    cout << "Delete after a : ";
+    printInfo(L);
+    return 0;
 }
